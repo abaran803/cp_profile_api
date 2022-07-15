258e6660248b3761de2ecdf1464a2c54e317e410
Ayush Baranwal <71405368+abaran803@users.noreply.github.com>
2022-07-15 11:13:36 +0530

    Adding Dynamic Route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', routes);
 
